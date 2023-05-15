4d2fab98a088849f527c7935dc9b76e970cc1015
matsaina <matsaina@gmail.com>
2023-05-15 12:57:21 +0300

    complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+
+def greet_programmer 
+puts "Hello, programmer!"
+end
+
+def greet name
+puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  
+
+def add num1,num2
+num1 + num2
+end
+
+def halve(half)
+    return half / 2 if half.is_a?(Integer)
+    nil
+end
+  
